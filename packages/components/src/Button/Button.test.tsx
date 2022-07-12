bb88486f8e91f6bc9548fa0aa11ad5e7010e1b47
mariyappan_condenast <murali_mariyappan@condenast.com>
2022-07-12 11:38:16 +0530

    feat(workflow): new env

--- packages/components/src/Button/Button.test.tsx
+++ packages/components/src/Button/Button.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Button from './Button';
 
-test('render a  label', (): void => {
+test('render a label', (): void => {
   const wrapper = shallow(<Button />);
 
   expect(wrapper).toMatchSnapshot();
