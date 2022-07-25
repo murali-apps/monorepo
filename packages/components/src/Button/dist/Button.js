a11758e2689b800efcb7eaf6b5be784de896f287
Murali Mariyappan <98752573+murali-condenast@users.noreply.github.com>
2022-07-25 18:36:28 +0530

    Revert "Revert "Revert "update"""

--- packages/components/src/Button/dist/Button.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-var Button = function () { return react_1["default"].createElement("button", { type: "submit" }, "Button 7"); };
-exports["default"] = Button;
