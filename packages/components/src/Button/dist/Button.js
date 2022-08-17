65870c7b72f7f5dbf921b4ba90feb440368b4d07
mariyappan_condenast <murali_mariyappan@condenast.com>
2022-08-17 09:09:53 +0530

    update

--- packages/components/src/Button/dist/Button.js
+++ packages/components/src/Button/dist/Button.js
@@ -1,5 +1,5 @@
 "use strict";
 exports.__esModule = true;
 var react_1 = require("react");
-var Button = function () { return react_1["default"].createElement("button", { type: "submit" }, "Button 7"); };
+var Button = function () { return react_1["default"].createElement("button", { type: "submit" }, "Button 8"); };
 exports["default"] = Button;
